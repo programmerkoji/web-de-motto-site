39f0ecb6fa2ffe693a3cc37e661290eba45d7aac
Koji Sato <utautaouka@gmail.com>
2026-04-18 23:51:04 +0900

    astro/sitemapを導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { loadEnv } from 'vite';
 
 const env = loadEnv('', '.', '');
 
 export default defineConfig({
+  integrations: [sitemap()],
   site: env.PUBLIC_SITE_URL || 'https://example.com',
   vite: {
     plugins: [tailwindcss()],
